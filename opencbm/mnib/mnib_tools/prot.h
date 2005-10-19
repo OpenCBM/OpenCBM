6b27e5e613e8ddc4bc6a24900b62d79cfe98ff47
Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
2005-10-19 08:21:45 +0000

    Removed files from mnib/mnib_tools/ that are also present at mnib/.

--- opencbm/mnib/mnib_tools/prot.h
+++
@@ -1,4 +0,0 @@
-void shift_buffer_left(BYTE *buffer, int length, int n);
-void shift_buffer_right(BYTE *buffer, int length, int n);
-BYTE* align_vmax(BYTE *work_buffer, int track_len);
-BYTE* align_vorpal(BYTE *work_buffer, int track_len);
--- opencbm/mnib/mnib_tools/version.h
+++
@@ -1,3 +0,0 @@
-#ifndef VERSION
-#define VERSION "0.36 build 091205 (rittwage)"
-#endif
