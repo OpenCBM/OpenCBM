4b3b09e941ac26b6bffa84b56c745a17db53d9f8
Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
2006-04-19 17:20:07 +0000

    Make sure all relevant files are deleted.

--- opencbm/internal/scripts/windistclean.sh
+++ opencbm/internal/scripts/windistclean.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 [ -e sys/libcommon/cbmlog.h ] || exit 1
 
-rm sys/libcommon/cbmlog.rc sys/libcommon/cbmlog.h
-rm `find . -name \*.inc|grep -iv makefile` `find . -name \*.plg`
-rm -r `find  . -name \*chk\*` `find . -name \*fre\*` bin/
+rm sys/libcommon/cbmlog.rc sys/libcommon/cbmlog.h sys/libcommon/msg00001.bin sys/libcommon/msg00002.bin
+rm `find . -name \*.inc|grep -iv makefile` `find . -name \*.plg` `find . -name \*.bsc` WINDOWS/cbm4win-vice.ncb WINDOWS/cbm4win-vice.opt WINDOWS/cbm4win.ncb WINDOWS/cbm4win.opt build*.log
+rm -r `find  . -name objchk\*` `find . -name objfre\*` bin/ Debug/ Release/
