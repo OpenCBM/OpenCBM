1eb96f60b3105391694a35db1c8ac261b51a3667
Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
2006-04-21 14:29:30 +0000

    Changed Copyright to reflect 2006.

--- opencbm/include/WINDOWS/version.common.h
+++ opencbm/include/WINDOWS/version.common.h
@@ -1,4 +1,4 @@
-/* $Id: version.common.h,v 1.2 2005-04-17 15:32:17 strik Exp $ */
+/* $Id: version.common.h,v 1.3 2006-04-21 14:29:30 strik Exp $ */
 
 #include "version.h"
 
@@ -7,7 +7,7 @@
 #undef VER_PRODUCTVERSION_STR
 #undef VER_COMPANYNAME_STR
 
-#define VER_LEGALCOPYRIGHT_STR      "(c) 2001-2005 Spiro Trikaliotis, based on cbm4linux, (c) 1999-2005 by Michael Klein"
+#define VER_LEGALCOPYRIGHT_STR      "(c) 2001-2006 Spiro Trikaliotis, based on cbm4linux, (c) 1999-2005 by Michael Klein, and others"
 #define VER_COMPANYNAME_STR         "(Spiro Trikaliotis, private)"
 
 #define VER_PRODUCTVERSION          CBM4WIN_VERSION_MAJOR,CBM4WIN_VERSION_MINOR,CBM4WIN_VERSION_SUBMINOR,CBM4WIN_VERSION_DEVEL
