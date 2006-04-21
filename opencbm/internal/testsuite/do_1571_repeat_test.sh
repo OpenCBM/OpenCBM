4df6744904f15581a9944740cd380b7bf3640042
Wolfgang Moser <http://d81.de/>
2006-04-21 15:30:13 +0000

    Some additions as a proposion on how this script can create its testimage on its own, when the needed tools (cbmconvert) are available and working. This extension is currently disabled.

--- opencbm/internal/testsuite/do_1571_repeat_test.sh
+++ opencbm/internal/testsuite/do_1571_repeat_test.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-# $Id: do_1571_repeat_test.sh,v 1.3 2006-04-20 18:19:43 wmsr Exp $
+# $Id: do_1571_repeat_test.sh,v 1.4 2006-04-21 15:30:13 wmsr Exp $
 
 # Before starting this script, do a:
 #
 #	d64copy -2 tstimg_rcmp_1571_-ts2.d71 $DRIVENO
+#
+# not needed anymore, it does it on its own now
+#
+
+DRIVETYPE=1571
+
+which c1541 2> /dev/null | fgrep c1541 > /dev/null
+if [ 0 -ne $? ]
+then
+	echo "VICE's c1541 is a prerequisite needed (in the search path)"
+	echo "for generating the test set as a D71 image file"
+	exit 1
+fi
+
+	# create the test fileset which is needed to generate the D71 image
+	# ./checkNcreateTestData.sh
+./checkNcreateMassData.sh
+
+# Doesn't work currently
+# cbmconvert -n -D7o tstimg_rcmp_1571.d71 cbmcopy_files/1[0-9][0-9][0-9]-15817141435.prg
+
+
+DRIVENO=`cbmctrl detect | fgrep " $DRIVETYPE " | cut -d: -f1 | tail -n 1 | tr -d "[:space:]"`
+
+# Doesn't work currently
+# d64copy -2 tstimg_rcmp_1571.d71 $DRIVENO
 
 function numargs {
 	if [ -e $1 ]
@@ -27,5 +53,5 @@ echo Logfile base name is: "$LOGFILEBASENAME"
 
 for (( i=1001 ; i<=9999 ; i=i+1 ))
 do
-	cbmcopy_rcmp.sh fill 1571 -ts2 2>&1 | tee $LOGFILEBASENAME$i.log
+	cbmcopy_rcmp.sh fill $DRIVETYPE -ts2 2>&1 | tee $LOGFILEBASENAME$i.log
 done
