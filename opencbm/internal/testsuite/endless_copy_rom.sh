e7f93ac45cd63f172d16c4a94b531e6991188cb9
Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
2006-05-06 13:34:29 +0000

    Drive is configurable now.

--- opencbm/internal/testsuite/endless_copy_rom.sh
+++ opencbm/internal/testsuite/endless_copy_rom.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# $Id: endless_copy_rom.sh,v 1.1 2006-02-12 13:41:04 strik Exp $
+# $Id: endless_copy_rom.sh,v 1.2 2006-05-06 13:34:29 strik Exp $
 
 clear
 
@@ -8,23 +8,24 @@ FC=`which fc`
 
 ROMSTART=0x8000
 ROMLEN=0x8000
+DRIVE=8
 
 [ -f COMPARE.ROM.TEST ] && rm COMPARE.ROM.TEST
 
 echo -e "\nDOWNLOADING initial ROM (for compare reasons)"
 date
-./cbmctrl download 8 $ROMSTART $ROMLEN COMPARE.ROM.TEST
+./cbmctrl download $DRIVE $ROMSTART $ROMLEN COMPARE.ROM.TEST
 date
 
 while true; do
 	[ -f read.rom.test ] && rm read.rom.test
 	echo -e "\nDOWNLOADING"
 	date
-	./cbmctrl download 8 $ROMSTART $ROMLEN read.rom.test
+	./cbmctrl download $DRIVE $ROMSTART $ROMLEN read.rom.test
 	date
 	$FC /b COMPARE.ROM.TEST read.rom.test
 	echo -e "\nUPLOADING (does not make sense, but anyway...)"
 	date
-	./cbmctrl upload 8 $ROMSTART COMPARE.ROM.TEST
+	./cbmctrl upload $DRIVE $ROMSTART COMPARE.ROM.TEST
 	date
 done
