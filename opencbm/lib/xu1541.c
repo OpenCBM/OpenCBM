be1b44a93157a7b2ee2dab078ecfaeee15cc6a1f
Till Harbaum <till@harbaum.org>
2007-02-20 13:36:35 +0000

    Complain about xu1541 in boot mode

--- opencbm/lib/xu1541.c
+++ opencbm/lib/xu1541.c
@@ -11,7 +11,7 @@
 /*! ************************************************************** 
 ** \file lib/xu1541.c \n
 ** \author Till Harbaum \n
-** \version $Id: xu1541.c,v 1.1.2.1 2007-02-08 19:19:40 harbaum Exp $ \n
+** \version $Id: xu1541.c,v 1.1.2.2 2007-02-20 13:36:35 harbaum Exp $ \n
 ** \n
 ** \brief libusb based xu1541 access routines
 **
@@ -20,11 +20,45 @@
 #ifdef ENABLE_XU1541 
 
 #include <stdio.h>
+#include <string.h>
 
 #include "opencbm.h"
 #include "xu1541.h"
 
 usb_dev_handle *xu1541_handle = NULL;
+
+static int  usbGetStringAscii(usb_dev_handle *dev, int index, int langid, 
+			      char *buf, int buflen)
+{
+  char    buffer[256];
+  int     rval, i;
+
+  if((rval = usb_control_msg(dev, USB_ENDPOINT_IN, 
+			     USB_REQ_GET_DESCRIPTOR, 
+			     (USB_DT_STRING << 8) + index, 
+			     langid, buffer, sizeof(buffer), 1000)) < 0)
+    return rval;
+
+  if(buffer[1] != USB_DT_STRING)
+    return 0;
+
+  if((unsigned char)buffer[0] < rval)
+    rval = (unsigned char)buffer[0];
+  
+  rval /= 2;
+  /* lossy conversion to ISO Latin1 */
+  for(i=1;i<rval;i++){
+    if(i > buflen)  /* destination buffer overflow */
+      break;
+    buf[i-1] = buffer[2 * i];
+    if(buffer[2 * i + 1] != 0)  /* outside of ISO Latin1 range */
+      buf[i-1] = '?';
+  }
+  buf[i-1] = 0;
+  return i-1;
+}
+
+#include <errno.h>
 
 /* try to find a xu1541 cable */
 int xu1541_init(void) {
@@ -37,12 +71,18 @@ int xu1541_init(void) {
   
   usb_find_busses();
   usb_find_devices();
-  
+
+  /* usb_find_devices sets errno if some devices reply 100% correct. */
+  /* make lib ignore this as this has nothing to do with our device */
+  errno = 0;
+
   for(bus = usb_get_busses(); bus; bus = bus->next) {
     for(dev = bus->devices; dev; dev = dev->next) {
       if((dev->descriptor.idVendor == XU1541_VID) &&
          (dev->descriptor.idProduct == XU1541_PID)) {
-
+	char    string[256];
+	int     len;
+	
         DEBUGF("Found xu1541 device on bus %s device %s.",
                bus->dirname, dev->filename);
 
@@ -50,16 +90,43 @@ int xu1541_init(void) {
         if(!(xu1541_handle = usb_open(dev)))
           fprintf(stderr, "Error: Cannot open USB device: %s\n",
                   usb_strerror());
+	
+	/* get device name and make sure the name is "xu1541" meaning */
+	/* that the device is not in boot loader mode */
+	len = usbGetStringAscii(xu1541_handle, dev->descriptor.iProduct, 
+				0x0409, string, sizeof(string));
+	if(len < 0){
+	  fprintf(stderr, "warning: cannot query product "
+		  "name for device: %s\n", usb_strerror());
+	  if(xu1541_handle) usb_close(xu1541_handle);
+	  xu1541_handle = NULL;
+	}
 
+	/* make sure the name matches what we expect */
+	if(strcmp(string, "xu1541") != 0) {
+	  fprintf(stderr, "Error: Found xu1541 in unexpected state,"
+		  " please make sure device is _not_ in bootloader mode!\n");
+	  
+	  if(xu1541_handle) usb_close(xu1541_handle);
+	  xu1541_handle = NULL;
+	}
+	
         break;
       }
     }
   }
-
-
+  
   if(!xu1541_handle) {
     DEBUGF("No xu1541 device found");
     return -1;
+  } else {
+#ifndef WIN
+    /* Get exclusive access to interface 0. Does not work under windows. */
+    if (usb_claim_interface(xu1541_handle, 0) != 0) {
+      fprintf(stderr, "USB error: %s\n", usb_strerror());
+      return -1;
+    }
+#endif
   }
 
   return 0;
@@ -68,6 +135,12 @@ int xu1541_init(void) {
 void xu1541_close(void)
 {
     DEBUGF("Closing USB link");
+
+#ifndef WIN
+    if(usb_release_interface(xu1541_handle, 0))
+      fprintf(stderr, "USB error: %s\n", usb_strerror());
+#endif
+
     usb_close(xu1541_handle);
 }
 
