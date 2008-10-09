10b0099a63fe887940f248cd83cf4222bbed5771
Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
2008-10-09 18:55:30 +0000

    Removed superfluous "break;" instructions; removed spaces and tabs before LF.

--- xu1541/firmware/main.c
+++ xu1541/firmware/main.c
@@ -4,10 +4,14 @@
  * Tabsize: 4
  * Copyright: (c) 2005 by Till Harbaum <till@harbaum.org>
  * License: GPL
- * This Revision: $Id: main.c,v 1.16 2008-10-09 18:03:00 strik Exp $
+ * This Revision: $Id: main.c,v 1.17 2008-10-09 18:55:30 strik Exp $
  *
  * $Log: main.c,v $
- * Revision 1.16  2008-10-09 18:03:00  strik
+ * Revision 1.17  2008-10-09 18:55:30  strik
+ * Removed superfluous "break;" instructions;
+ * removed spaces and tabs before LF.
+ *
+ * Revision 1.16  2008/10/09 18:03:00  strik
  * Removed references to AVRUSB (usbconfig.h) and USBTINY in the firmware.
  *
  * Revision 1.15  2007/08/30 18:50:15  strik
@@ -135,11 +139,11 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
 /*  static uchar replyBuf[4]; */
   char talk;
   uchar len = data[2];
-  
+
   DEBUGF("cmd %d (%d)\n", data[1], data[1] - XU1541_IOCTL);
-  
+
   switch(data[1]) {
-    
+
     /* ----- USB testing ----- */
   case XU1541_IRQ_PAUSE:
     xu1541_req_irq_pause(len);
@@ -149,8 +153,7 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
     DEBUGF("echo\n");
     memcpy(replyBuf, data+2, 4);
     return 4;
-    break;
-    
+
     /* ----- Debugging ----- */
 #ifdef ENABLE_EVENT_LOG
   case XU1541_GET_EVENT:
@@ -158,9 +161,8 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
     replyBuf[0] = EVENT_LOG_LEN;
     replyBuf[1] = event_log_get(len);
     return 2;
-    break;
 #endif
-    
+
     /* ----- Basic I/O ----- */
   case XU1541_INFO:
     replyBuf[0] = XU1541_VERSION_MAJOR;
@@ -168,45 +170,37 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
     *(ushort*)(replyBuf+2) = XU1541_CAPABILIIES;
     *(ushort*)(replyBuf+4) = pgm_read_word_near(&bios_data.version_major);
     return 6;
-    break;
 
   case XU1541_FLASH:
     bios_start_flash_bootloader();
     return 0;
-    break;
-      
+
   case XU1541_EEPROM_READ:
     io_mode = MODE_EEPROM;
     return(data[2]?0xff:0x00);
-    break;
 
   case XU1541_EEPROM_WRITE:
     io_mode = MODE_EEPROM;
     return(data[2]?0xff:0x00);
-    break;
-    
+
   case XU1541_REQUEST_READ:
     xu1541_request_read(len);
     return 0;
-    break;
 
   case XU1541_GET_RESULT:
     xu1541_get_result(replyBuf);
     return 2;
-    break;
-    
+
   case XU1541_READ:
     io_mode = MODE_ORIGINAL;
     DEBUGF("rd %d\n", data[2]);
     /* more data to be expected? */
     return(data[2]?0xff:0x00);
-    break;
-    
+
   case XU1541_WRITE:
     io_mode = MODE_ORIGINAL;
     xu1541_prepare_write(data[2]);
     return(data[2]?0xff:0x00);
-    break;
 
   case XU1541_TALK:
   case XU1541_LISTEN:
@@ -216,16 +210,14 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
     data[3] |= 0x60;
     xu1541_request_async(data+2, 2, 1, talk);
     return 0;
-    break;
-    
+
   case XU1541_UNTALK:
   case XU1541_UNLISTEN:
     DEBUGF("untlk/unlst()\n");
     data[2] = (data[1] == XU1541_UNTALK) ? 0x5f : 0x3f;
     xu1541_request_async(data+2, 1, 1, 0);
     return 0;
-    break;
-    
+
   case XU1541_OPEN:
   case XU1541_CLOSE:
     DEBUGF("open/close(%d,%d)\n", data[2], data[3]);
@@ -233,54 +225,46 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
     data[3] |= (data[1] == XU1541_OPEN) ? 0xf0 : 0xe0;
     xu1541_request_async(data+2, 2, 1, 0);
     return 0;
-    break;
-    
+
   case XU1541_GET_EOI:
     replyBuf[0] = eoi ? 1 : 0;
     return 1;
-    break;
-    
+
   case XU1541_CLEAR_EOI:
     eoi = 0;
     return 0;
-    break;
-    
+
   case XU1541_RESET:
     LED_OFF();
     DEBUGF("rst\n");
     do_reset();
     return 0;
-    break;
 
     /* ----- Low-level port access ----- */
-    
+
   case XU1541_IEC_WAIT:
     xu1541_wait(data[2], data[3]);
     /* intentional fall through */
-    
+
   case XU1541_IEC_POLL:
     replyBuf[0] = xu1541_poll();
     DEBUGF("poll=%x\n", replyBuf[0]);
     return 1;
-    break;
-      
+
   case XU1541_IEC_SETRELEASE:
     xu1541_setrelease(data[2], data[3]);
     return 0;
-    break;
-    
+
   case XU1541_PP_READ:
     replyBuf[0] = xu1541_pp_read();
     return 1;
-    break;
-    
+
   case XU1541_PP_WRITE:
     xu1541_pp_write(len);
     return 0;
-    break;
-    
-    /* ----- special protocol drivers access ----- */    
-    
+
+    /* ----- special protocol drivers access ----- */
+
     /* ----- S1 ----- */
   case XU1541_S1:
     switch(data[2]) {
@@ -288,26 +272,22 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
       replyBuf[0] = S1_VERSION_MAJOR;
       replyBuf[1] = S1_VERSION_MINOR;
       return 2;
-      break;
 
     case XU1541_READ:
       io_mode = MODE_S1;
       /* more data to be expected? */
       return(data[4]?0xff:0x00);
-      break;
-      
+
     case XU1541_WRITE:
       io_mode = MODE_S1;
       return(data[4]?0xff:0x00);
-      break;
-      
-      break;
+
     default:
       DEBUGF("ERR: s1 cmd %d not impl.\n", data[1]);
       break;
     }
     break;
-    
+
     /* ----- S2 ----- */
   case XU1541_S2:
     switch(data[2]) {
@@ -315,26 +295,22 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
       replyBuf[0] = S2_VERSION_MAJOR;
       replyBuf[1] = S2_VERSION_MINOR;
       return 2;
-      break;
-      
+
     case XU1541_READ:
       io_mode = MODE_S2;
       /* more data to be expected? */
       return(data[4]?0xff:0x00);
-      break;
-      
+
     case XU1541_WRITE:
       io_mode = MODE_S2;
       return(data[4]?0xff:0x00);
-      break;
-      
-      break;
+
     default:
       DEBUGF("ERR: s2 cmd %d not impl.\n", data[1]);
       break;
     }
     break;
-    
+
     /* ----- PP ----- */
   case XU1541_PP:
     switch(data[2]) {
@@ -342,26 +318,22 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
       replyBuf[0] = PP_VERSION_MAJOR;
       replyBuf[1] = PP_VERSION_MINOR;
       return 2;
-      break;
-      
+
     case XU1541_READ:
       io_mode = MODE_PP;
       /* more data to be expected? */
       return(data[4]?0xff:0x00);
-      break;
-      
+
     case XU1541_WRITE:
       io_mode = MODE_PP;
       return(data[4]?0xff:0x00);
-      break;
-	  
-      break;
+
     default:
       DEBUGF("ERR: pp cmd %d not impl.\n", data[1]);
       break;
     }
     break;
-    
+
     /* ----- P2 ----- */
   case XU1541_P2:
     switch(data[2]) {
@@ -369,32 +341,28 @@ uchar	usbFunctionSetup(uchar data[8], byte_t *replyBuf) {
       replyBuf[0] = P2_VERSION_MAJOR;
       replyBuf[1] = P2_VERSION_MINOR;
       return 2;
-      break;
-      
+
     case XU1541_READ:
       io_mode = MODE_P2;
       /* more data to be expected? */
       return(data[4]?0xff:0x00);
-      break;
-      
+
     case XU1541_WRITE:
       io_mode = MODE_P2;
       return(data[4]?0xff:0x00);
-      break;
-      
-      break;
+
     default:
       DEBUGF("ERR: p2 cmd %d not impl.\n", data[1]);
       break;
-      }
+    }
     break;
-      
+
   default:
     DEBUGF("ERR: cmd %d not impl.\n", data[0]);
     // must not happen ...
     break;
   }
-  
+
   return 0;  // reply len
 }
 
@@ -408,7 +376,7 @@ uchar usbFunctionRead( uchar *data, uchar len )
   char rv = 0;
 
   DEBUGF("rd %d\n", len);
-  LED_ON();  
+  LED_ON();
 
   switch(io_mode) {
     case MODE_ORIGINAL:
@@ -437,7 +405,7 @@ uchar usbFunctionRead( uchar *data, uchar len )
       break;
   }
 
-  LED_OFF();  
+  LED_OFF();
   return (rv>0)?rv:0;
 }
 
@@ -447,8 +415,8 @@ uchar usbFunctionRead( uchar *data, uchar len )
 
 uchar usbFunctionWrite( uchar *data, uchar len )
 {
-  char rv = 0;  
-  LED_ON();  
+  char rv = 0;
+  LED_ON();
 
   switch(io_mode) {
     case MODE_ORIGINAL:
@@ -477,7 +445,7 @@ uchar usbFunctionWrite( uchar *data, uchar len )
       break;
   }
 
-  LED_OFF();  
+  LED_OFF();
   return (rv>0)?rv:0;
 }
 
@@ -508,7 +476,7 @@ int	main(void) {
     EVENT(EVENT_RESET_BROWNOUT);
     MCUCSR &= ~_BV(BORF);
   }
-#endif  
+#endif
 
 
 #if DEBUG_LEVEL > 0
@@ -526,7 +494,7 @@ int	main(void) {
   LED_ON();           /* drive led during startup */
 
   /* rxd (d.0) is iec_srq. we don't use it (yet) so just make it an input */
-  DDRD &= ~_BV(0);     /* pin is input */  
+  DDRD &= ~_BV(0);     /* pin is input */
 #endif
 #endif
 
