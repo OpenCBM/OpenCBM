bc5e6714b0ea8ce4d8d648a42ce44a5fe0a10631
Wolfgang Moser <http://d81.de/>
2010-01-07 00:32:39 +0000

    Specialisation for reliable xum1541 board reset.

--- opencbm/internal/testsuite/killshll.sh
+++ opencbm/internal/testsuite/killshll.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# $Id: killshll.sh,v 1.1 2006-04-12 23:38:20 wmsr Exp $
+# $Id: killshll.sh,v 1.2 2010-01-07 00:32:39 wmsr Exp $
 
 if [ -f shelltst.pid ]
 then
   UXPID=`cat shelltst.pid`
 
 # The following lines need a special utility for Windows
-#
-#  WINPID=`ps ux | fgrep $UXPID | tail -n 1 | cut -c26-36`
-#  echo "Sending SIGINT (CTRL-C) to Windows console process $WINPID"
-#  sendCTRL-C $WINPID
-#  sleep 5
+
+  WINPID=`ps ux | fgrep $UXPID | tail -n 1 | cut -c26-36`
+  echo "Sending SIGINT (CTRL-C) to Windows console process $WINPID"
+  sendCTRL-C $WINPID
+  sleep 5
 
   ps ux | fgrep $UXPID > /dev/null
   if [ $? -eq 0 ]
@@ -22,5 +22,7 @@ then
   fi
   rm -f shelltst.pid
 fi
-echo "Doing reset on IEC bus"
-cbmctrl reset
+# echo "Doing reset on IEC bus"
+# cbmctrl reset
+echo "Doing reset of the xum1541 firmware including IEC bus"
+avrdude -c avrisp2 -P usb -p usb1287 -nuqq
