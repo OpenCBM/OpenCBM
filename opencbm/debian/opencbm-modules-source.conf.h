004adce0654301f38c7587ff18c5496c2183cdf6
Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
2011-12-19 21:45:06 +0100

    Rename debian packages to OpenCBM.

--- opencbm/debian/opencbm-modules-source.conf.h
+++ opencbm/debian/opencbm-modules-source.conf.h
@@ -1,4 +1,4 @@
-/* compile-time config file for the cbm4linux kernel module */
+/* compile-time config file for the opencbm kernel module */
 
 /* port access mode (ignored for 2.0.x kernels) */
 #undef DIRECT_PORT_ACCESS
