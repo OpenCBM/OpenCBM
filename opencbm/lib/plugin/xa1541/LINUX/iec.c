ca4c06e7430caf3b2296d4e1e00051fff015fc53
Felix Palmen <felix@palmen-it.de>
2018-11-25 14:57:57 +0100

    change ioctl interface to declare reads and writes

--- opencbm/lib/plugin/xa1541/LINUX/iec.c
+++ opencbm/lib/plugin/xa1541/LINUX/iec.c
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include "opencbm.h"
 #include "cbm_module.h"
@@ -99,88 +100,100 @@ int opencbm_plugin_raw_read(CBM_FILE f, void *buf, size_t size)
 
 int opencbm_plugin_listen(CBM_FILE f, unsigned char dev, unsigned char secadr)
 {
-    return ioctl(f, CBMCTRL_LISTEN, (dev<<8) | secadr);
+    int arg = (dev<<8) | secadr;
+    return ioctl(f, CBMCTRL_LISTEN, &arg) ? -errno : 0;
 }
 
 int opencbm_plugin_talk(CBM_FILE f, unsigned char dev, unsigned char secadr)
 {
-    return ioctl(f, CBMCTRL_TALK, (dev<<8) | secadr);
+    int arg = (dev<<8) | secadr;
+    return ioctl(f, CBMCTRL_TALK, &arg) ? -errno : 0;
 }
 
 int opencbm_plugin_open(CBM_FILE f, unsigned char dev, unsigned char secadr)
 {
-    int rv;
-
-    rv = ioctl(f, CBMCTRL_OPEN, (dev<<8) | secadr);
-    return rv;
+    int arg = (dev<<8) | secadr;
+    return ioctl(f, CBMCTRL_OPEN, &arg) ? -errno : 0;
 }
 
 int opencbm_plugin_close(CBM_FILE f, unsigned char dev, unsigned char secadr)
 {
-    return ioctl(f, CBMCTRL_CLOSE, (dev<<8) | secadr);
+    int arg = (dev<<8) | secadr;
+    return ioctl(f, CBMCTRL_CLOSE, &arg) ? -errno : 0;
 }
 
 int opencbm_plugin_unlisten(CBM_FILE f)
 {
-    return ioctl(f, CBMCTRL_UNLISTEN);
+    return ioctl(f, CBMCTRL_UNLISTEN) ? -errno : 0;
 }
 
 int opencbm_plugin_untalk(CBM_FILE f)
 {
-    return ioctl(f, CBMCTRL_UNTALK);
+    return ioctl(f, CBMCTRL_UNTALK) ? -errno : 0;
 }
 
 int opencbm_plugin_get_eoi(CBM_FILE f)
 {
-    return ioctl(f, CBMCTRL_GET_EOI);
+    int eoi;
+    if (ioctl(f, CBMCTRL_GET_EOI, &eoi)) return -errno;
+    return eoi;
 }
 
 int opencbm_plugin_clear_eoi(CBM_FILE f)
 {
-    return ioctl(f, CBMCTRL_CLEAR_EOI);
+    return ioctl(f, CBMCTRL_CLEAR_EOI) ? -errno : 0;
 }
 
 int opencbm_plugin_reset(CBM_FILE f)
 {
-    return ioctl(f, CBMCTRL_RESET);
+    return ioctl(f, CBMCTRL_RESET) ? -errno : 0;
 }
 
 unsigned char opencbm_plugin_pp_read(CBM_FILE f)
 {
-    return ioctl(f, CBMCTRL_PP_READ);
+    unsigned char val;
+    if (ioctl(f, CBMCTRL_PP_READ, &val)) return -errno;
+    return val;
 }
 
 void opencbm_plugin_pp_write(CBM_FILE f, unsigned char c)
 {
-    ioctl(f, CBMCTRL_PP_WRITE, c);
+    ioctl(f, CBMCTRL_PP_WRITE, &c);
 }
 
 int opencbm_plugin_iec_poll(CBM_FILE f)
 {
-    return ioctl(f, CBMCTRL_IEC_POLL);
+    int val;
+    if (ioctl(f, CBMCTRL_IEC_POLL, &val)) return -errno;
+    return val;
 }
 
 int opencbm_plugin_iec_get(CBM_FILE f, int line)
 {
-    return (ioctl(f, CBMCTRL_IEC_POLL) & line) != 0;
+    int val;
+    if (ioctl(f, CBMCTRL_IEC_POLL, &val)) return -errno;
+    return (val & line) != 0;
 }
 
 void opencbm_plugin_iec_set(CBM_FILE f, int line)
 {
-    ioctl(f, CBMCTRL_IEC_SET, line);
+    ioctl(f, CBMCTRL_IEC_SET, &line);
 }
 
 void opencbm_plugin_iec_release(CBM_FILE f, int line)
 {
-    ioctl(f, CBMCTRL_IEC_RELEASE, line);
+    ioctl(f, CBMCTRL_IEC_RELEASE, &line);
 }
 
 int opencbm_plugin_iec_wait(CBM_FILE f, int line, int state)
 {
-    return ioctl(f, CBMCTRL_IEC_WAIT, (line<<8) | state);
+    int arg = (line<<8) | state;
+    if (ioctl(f, CBMCTRL_IEC_WAIT, &arg)) return -errno;
+    return arg;
 }
 
 void opencbm_plugin_iec_setrelease(CBM_FILE f, int set, int release)
 {
-    ioctl(f, CBMCTRL_IEC_SETRELEASE, (set<<8) | release);
+    int arg = (set<<8) | release;
+    ioctl(f, CBMCTRL_IEC_SETRELEASE, &arg);
 }
--- opencbm/lib/plugin/xa1541/LINUX/parburst.c
+++ opencbm/lib/plugin/xa1541/LINUX/parburst.c
@@ -30,46 +30,36 @@
 
 unsigned char opencbm_plugin_parallel_burst_read(CBM_FILE f)
 {
-    return ioctl(f, CBMCTRL_PARBURST_READ);
+    unsigned char val;
+    if (ioctl(f, CBMCTRL_PARBURST_READ, &val)) return -errno;
+    return val;
 }
 
 void opencbm_plugin_parallel_burst_write(CBM_FILE f, unsigned char c)
 {
-    ioctl(f, CBMCTRL_PARBURST_WRITE, c);
+    ioctl(f, CBMCTRL_PARBURST_WRITE, &c);
 }
 
 int opencbm_plugin_parallel_burst_read_track(CBM_FILE f, unsigned char *buffer, unsigned int length)
 {
-    int retval;
-
     PARBURST_RW_VALUE mv;
     mv.buffer=buffer;
     mv.length=length; /* only needed in write_track */
-    retval=ioctl(f, CBMCTRL_PARBURST_READ_TRACK, &mv);  /* we have to catch retval to check on EFAULT */
-    if(retval==(-EFAULT)) {printf("cbm4linux: cbm.c: cbm_parallel_burst_read_track: ioctl returned -EFAULT"); return 0;}
-    return retval;
+    return ioctl(f, CBMCTRL_PARBURST_READ_TRACK, &mv) ? -errno : 0;
 }
 
 int opencbm_plugin_parallel_burst_read_track_var(CBM_FILE f, unsigned char *buffer, unsigned int length)
 {
-    int retval;
-
     PARBURST_RW_VALUE mv;
     mv.buffer=buffer;
     mv.length=length; /* only needed in write_track */
-    retval=ioctl(f, CBMCTRL_PARBURST_READ_TRACK_VAR, &mv);  /* we have to catch retval to check on EFAULT */
-    if(retval==(-EFAULT)) {printf("cbm4linux: cbm.c: cbm_parallel_burst_read_track_var: ioctl returned -EFAULT"); return 0;}
-    return retval;
+    return ioctl(f, CBMCTRL_PARBURST_READ_TRACK_VAR, &mv) ? -errno : 0;
 }
 
 int opencbm_plugin_parallel_burst_write_track(CBM_FILE f,  unsigned char *buffer, unsigned int length)
 {
-    int retval;
-
     PARBURST_RW_VALUE mv;
     mv.buffer=buffer;
     mv.length=length;
-    retval=ioctl(f, CBMCTRL_PARBURST_WRITE_TRACK, &mv);
-    if(retval==(-EFAULT)) {printf("cbm4linux: cbm.c: cbm_parallel_burst_write_track: ioctl returned -EFAULT"); return 0;}
-    return retval;
+    return ioctl(f, CBMCTRL_PARBURST_WRITE_TRACK, &mv) ? -errno : 0;
 }
